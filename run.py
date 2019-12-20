ec420fe8bd2c2d30f75df0471c1adfc46fc48409
Ferhat <47542683+FerhatYilmaz1986@users.noreply.github.com>
2019-12-20 09:49:17 -0500

    Feature : Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 # /run.py
 import os
-from dotenv import load_dotenv, find_dotenv
+
 
 from src.app import create_app
 
-load_dotenv(find_dotenv())
+
 
 env_name = os.getenv('FLASK_ENV')
 app = create_app(env_name)
